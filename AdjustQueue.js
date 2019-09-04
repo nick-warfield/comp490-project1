e563d6a7e4cf5d27b8c686a763f9a4e65813bc91
Nicholas Warfield <warfield9000@gmail.com>
2019-09-04 10:21:00 -0700

    More table changes Removed all the elements we don't need for showing off features. Started integrating confirmation num.

--- AdjustQueue.js
+++ AdjustQueue.js
@@ -1,28 +1,40 @@
+// generate numbers that are always 4 digits and unique
+class ConfirmNumber {
+
+	ConfirmNumber() {
+		min = 1000;
+		max = 9999;
+		confNum = min;
+
+		function genNum() {
+			var num = confNum;
+			confNum++;
+			if (confNum > max) { confNum = min; }
+			return num;
+		}
+	}
+
+	function get() { return genNum(); }
+}
+
 function addUser() {
 	var table = document.getElementById("queue");
 	var userName = document.getElementById("username").value;
-	var IDNum = document.getElementById("idnum").value;
-	var service = document.getElementById("service").value;
+	var email = document.getElementById("email").value;
 	var newRow = table.insertRow(row);
 	var row = table.rows.length;
-	var confNum = Math.floor(Math.random() * 100);
+	var confNum = numberGen.get();
 	
-	for (var c = 0; c < 6; c++) {
+	for (var c = 0; c < 3; c++) {
 		var newCell = newRow.insertCell(c);
 		if (c == 0) {
 			newCell.innerHTML = confNum;
 		}
 		if (c == 1) {
-			newCell.innerHTML = IDNum;
-		}
-		if (c == 2) {
 			newCell.innerHTML = userName;
 		}
-		if (c == 3 || c == 4) {
-			newCell.innerHTML = '-';
-		}
-		if (c == 5) {
-			newCell.innerHTML = service;
+		if (c == 2) {
+			newCell.innerHTML = email;
 		}
 	}
 	window.alert("Your Confirmation#: " + confNum);
