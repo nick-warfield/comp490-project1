f7996009e1a01b668c21a4aff158aa551744697e
Vincent Becerra <51965495+VinceBec@users.noreply.github.com>
2019-09-04 17:13:32 -0700

    removeUser

--- AdjustQueue.js
+++ AdjustQueue.js
@@ -1,33 +1,49 @@
+var min = 1000;
+var max = 9999;
+var confNum = min;
+
+function genNum() {
+	var num = confNum;
+	confNum++;
+	if (confNum > max) { confNum = min; }
+	return num;
+}
+
 function addUser() {
 	var table = document.getElementById("queue");
 	var userName = document.getElementById("username").value;
-	var IDNum = document.getElementById("idnum").value;
-	var service = document.getElementById("service").value;
+	var email = document.getElementById("email").value;
 	var newRow = table.insertRow(row);
 	var row = table.rows.length;
-	var confNum = Math.floor(Math.random() * 100);
-	
-	for (var c = 0; c < 6; c++) {
+	var confNum = genNum();
+
+	for (var c = 0; c < 3; c++) {
 		var newCell = newRow.insertCell(c);
 		if (c == 0) {
 			newCell.innerHTML = confNum;
 		}
 		if (c == 1) {
-			newCell.innerHTML = IDNum;
-		}
-		if (c == 2) {
 			newCell.innerHTML = userName;
 		}
-		if (c == 3 || c == 4) {
-			newCell.innerHTML = '-';
-		}
-		if (c == 5) {
-			newCell.innerHTML = service;
+		if (c == 2) {
+			newCell.innerHTML = email;
 		}
 	}
 	window.alert("Your Confirmation#: " + confNum);
 }
 
 function removeUser() {
-	
+	var removeNum = document.getElementById("confNum").value;
+	var table = document.getElementById("queue");
+	for (var i = 1; i < table.rows.length; i++){
+		var current = table.rows[i].cells[0].innerHTML;
+		if (removeNum === current) {
+			table.deleteRow(i);
+		}
+	}
+}
+
+function removeFront() {
+	var table = document.getElementById("queue");
+	table.deleteRow(1);
 }
